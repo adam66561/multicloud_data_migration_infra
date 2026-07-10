dec74556f3a98929ad9294406f885c969588c900
adam66561 <adam.kepczynski32@gmail.com>
2026-07-11 00:44:00 +0200

    added s3 backend for bootstrappping purpose

---
+++ bootstrapping/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  default_separator = "-"
+  account_id = data.aws_caller_identity.current.account_id
+  region = data.aws_region.current.region
+  prefix = join(local.default_separator, [var.env, var.project])
+  
+  common_tags = {
+    env         = var.env
+    project     = var.project
+    repository  = var.repositories[0]
+  }
+}
---
+++ bootstrapping/terraform.tfvars
@@ -0,0 +1,4 @@
+env = "dev"
+region = "eu-central-1"
+project = "multicloud"
+repositories = ["multicloud_data_migration_infra"]
